acdf453db0de233babb191bd7a38c148dceffc37
sama-hossam-prog <samahossam361@gmail.com>
2026-06-21 20:18:09 +0300

    Add files via upload

---
+++ classification.py
@@ -0,0 +1,29 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+
+# Load dataset
+data = load_iris()
+
+X = data.data
+y = data.target
+
+# Split data (training / testing)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+# Model
+model = DecisionTreeClassifier()
+
+# Train model
+model.fit(X_train, y_train)
+
+# Predict
+y_pred = model.predict(X_test)
+
+# Accuracy
+accuracy = accuracy_score(y_test, y_pred)
+
+print("Model Accuracy:", accuracy)
